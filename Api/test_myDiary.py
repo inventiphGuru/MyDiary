f217769b23c13921bb0b54c86e86d5e82d5a3188
Sammy ammy <appguru.sam@gmail.com>
2018-07-19 13:38:32 +0300

    Update test for getting one entry
    The test for fetching one entry has been updated to check for the
    response code.

--- Api/test_myDiary.py
+++ Api/test_myDiary.py
@@ -1,17 +1,24 @@
 from unittest import TestCase
-
+from flask import jsonify
 from MyDiary import MyDiary
 
 
 class TestMyDiary(TestCase):
     def test_get_all_entries(self):
-        result=
-        self.assertEqual(result["message"], "Entry made")
-        self.assertEqual(response.status_code, 201)
-        self.fail()
+        result = MyDiary.get_all_entries()
+        if len(result) > 0:
+            self.assertEqual(result['Message'], jsonify(result))
+        else:
+            self.assertEqual(result['Message'], 'No entries yet')
 
-    def test_get_one_entry(self):
-        self.fail()
+    def get_one_entry(self):
+        result = MyDiary.get_one_entry(MyDiary.get_one_entry, 2)
+        if len(result) > 0:
+            response = jsonify(result)
+            self.assertEqual(result['Message'], MyDiary.get_one_entry(MyDiary.get_one_entry, 2))
+            self.assertEqual(response.status_code, 200)
+        else:
+            self.assertEqual(result['Message'], 'No entries yet')
 
     def test_add_new_entry(self):
         self.fail()
