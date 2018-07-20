61c20a5e68875683fb4d8a76ef013daa17a52eac
Sammy ammy <appguru.sam@gmail.com>
2018-07-20 14:22:29 +0300

    update tests for all endpoints
    endpoint test missing class being called

--- Api/test_myDiary.py
+++ Api/test_myDiary.py
@@ -1,35 +1,35 @@
 from unittest import TestCase
 from flask import jsonify
-from MyDiary import MyDiary
+'''from MyDiary import '''
 
 
 class TestMyDiary(TestCase):
     def test_get_all_entries(self):
-        result = MyDiary.get_all_entries()
+        result = get_all_entries()
         if len(result) > 0:
             self.assertEqual(result['Message'], jsonify(result))
         else:
             self.assertEqual(result['Message'], 'No entries yet')
 
     def get_one_entry(self):
-        result = MyDiary.get_one_entry(MyDiary.get_one_entry, 2)
+        result = MyDiary.get_one_entry( 2)
         if len(result) > 0:
             response = jsonify(result)
-            self.assertEqual(result['Message'], MyDiary.get_one_entry(MyDiary.get_one_entry, 2))
+            self.assertEqual(result['Message'], get_one_entry(get_one_entry, 2))
             self.assertEqual(response.status_code, 200)
         else:
             self.assertEqual(result['Message'], 'No entries yet')
 
     def test_add_new_entry(self):
         #get record count
-        res = MyDiary.get_all_entries()
+        res = get_all_entries()
 
         #add new record
-        result = MyDiary.add_new_entry()
+        result = add_new_entry()
         response= jsonify(result)
 
         #get current record count after adding
-        res1 = MyDiary.get_all_entries()
+        res1 = get_all_entries()
 
         #check if the count of res is less than res1
         if len(res) < len(res1):
